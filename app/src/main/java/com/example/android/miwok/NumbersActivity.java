0b6c6e022fa9136ac4abdf5da5ed73b262c92335
Lauren Mwano <ellemwano@users.noreply.github.com>
2017-12-02 16:55:37 +0100

    Set array to ArrayList and Modify Log.v

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -11,20 +13,29 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        //Array of number words in English
-        String [] words = {"one", "two", "three", "four", "five",
-        "six", "seven", "eight", "nine", "ten"};
+        //ArrayList of number words in English
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
-        //Verify the content of the array by printing each array elements to the log
-        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
-        Log.v("NumbersActivity", "Word at index 1: " + words[1]);
-        Log.v("NumbersActivity", "Word at index 2: " + words[2]);
-        Log.v("NumbersActivity", "Word at index 3: " + words[3]);
-        Log.v("NumbersActivity", "Word at index 4: " + words[4]);
-        Log.v("NumbersActivity", "Word at index 5: " + words[5]);
-        Log.v("NumbersActivity", "Word at index 6: " + words[6]);
-        Log.v("NumbersActivity", "Word at index 7: " + words[7]);
-        Log.v("NumbersActivity", "Word at index 8: " + words[8]);
-        Log.v("NumbersActivity", "Word at index 9: " + words[9]);
+        //Verify the content of the ArrayList by printing each ArrayList elements to the log
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(0));
+        Log.v("NumbersActivity", "Word at index 1: " + words.get(1));
+        Log.v("NumbersActivity", "Word at index 2: " + words.get(2));
+        Log.v("NumbersActivity", "Word at index 3: " + words.get(3));
+        Log.v("NumbersActivity", "Word at index 4: " + words.get(4));
+        Log.v("NumbersActivity", "Word at index 5: " + words.get(5));
+        Log.v("NumbersActivity", "Word at index 6: " + words.get(6));
+        Log.v("NumbersActivity", "Word at index 7: " + words.get(7));
+        Log.v("NumbersActivity", "Word at index 8: " + words.get(8));
+        Log.v("NumbersActivity", "Word at index 9: " + words.get(9));
     }
 }
